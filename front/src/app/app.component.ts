caf28f4b99215ccab8779bfe977f75b4c37cd462
maricotnicolas <nicolasmaricot@yahoo.fr>
2024-03-10 17:08:26 -0300

    Component administrador gerente foi criado

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent,AdminComponent],
+    templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
