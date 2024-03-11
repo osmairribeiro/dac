9345483a9e2a043f9ccb5266e50cb8c0ebc237d1
Lucas Ribeiro <38088431+Lucasrcarvalho@users.noreply.github.com>
2024-03-10 21:03:34 -0300

    tela inicial de cliente

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent,AdminComponent],
+  imports: [
+    RouterOutlet, 
+    HomeComponent, 
+    AdminComponent,
+    ClienteComponent],
     templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'clientes', component: ClienteComponent },
+    { path: 'gerentes', component: AdminComponent },
+];
