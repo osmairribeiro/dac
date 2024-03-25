1e263861211d2a4576152f25e2134a066d4702f6
Maferibe <40439226+CapitaoFelix@users.noreply.github.com>
2024-03-25 14:25:31 -0300

    correção pastas

---
+++ dac-main/front/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { AutocadastroComponent } from './autocadastro/autocadastro.component';
+import { GerenteComponent } from './gerente/home/gerente.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports:[
+    RouterOutlet, 
+    HomeComponent, 
+    AdminComponent,
+    ClienteComponent,
+    AutocadastroComponent,
+    GerenteComponent
+  ],
+    templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = 'bantads';
+}
